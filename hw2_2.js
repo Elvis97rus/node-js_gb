757938f446f9e5bd5f17edfa1e4dbc3c63220826
Artem Shavkunov <shavkunov.a@seointellect.ru>
2021-10-27 10:56:30 +0300

    2_1 последовательность вывода в консоль
    2_2
    независимые таймеры обратного отсчета с выводом в консоль

--- hw2_2.js
+++ hw2_2.js
@@ -5,16 +5,21 @@ class TimerEmitter extends EventEmitter {
         super();
     }
 }
+
 class TimerHandler {
     static tick(name, payload) {
-        let numb = setInterval(() => {
-            TimerHandler.log(name, payload);
-            payload--;
+        if (Number.isInteger(payload)){
+            let numb = setInterval(() => {
+                TimerHandler.log(name, payload);
+                payload--;
 
-            if (payload < 0){
-                TimerHandler.stop(name, numb);
-            }
-        },1000);
+                if (payload < 0){
+                    TimerHandler.stop(name, numb);
+                }
+            },1000);
+        }else{
+            TimerHandler.log(name, payload);
+        }
     }
 
     static stop(name, intervalNumb) {
@@ -23,6 +28,10 @@ class TimerHandler {
     }
 
     static rangeToUnix(range) {
+        if ((range[0] > 23) || (range[1] > 30) || (range[2] > 12)) {
+            return new Error('!Input valid value!')
+        }
+
         let unixTime = 0;
         if (range[0]) {
             unixTime += range[0]*3600;
@@ -54,27 +63,37 @@ class Timer {
 const timerEmitter = new TimerEmitter();
 timerEmitter.on('tick', TimerHandler.tick);
 
-// получаем диапазон значений из консоли, при запуске файла (node ./hw2_2.js 1-0-0-0)
-const moreThanOneTimer = (process.argv[2].split(' ').length > 1);
-let range = (moreThanOneTimer)
-    ? process.argv[2].split(' ')
-    : process.argv[2].split('-');
+// Для запуска использовать консоль,
+// в качестве агрументов передавать 4 числа разделенных дефисом
+// последовательность - час - день - месяц - год
+// (node ./hw2_2.js 1-0-0-0) || (node ./hw2_2.js "1-0-0-0 2-0-0-0")
 
-const run = async () => {
-    let timerNu = 1;
-    if (moreThanOneTimer){
-        for (const el of range) {
-            const timer = await new Timer(
-                {
-                    name: `nu-${timerNu++}`,
-                    payload: TimerHandler.rangeToUnix(el.split('-'))
-                });
+if (process.argv[2]){
+    const moreThanOneTimer = (process.argv[2].split(' ').length > 1);
+    let range = (moreThanOneTimer)
+        ? process.argv[2].split(' ')
+        : process.argv[2].split('-');
+
+    const run = async () => {
+        let timerNu = 1;
+        if (moreThanOneTimer){
+            for (const el of range) {
+                const timer = await new Timer(
+                    {
+                        name: `nu-${timerNu++}`,
+                        payload: TimerHandler.rangeToUnix(el.split('-'))
+                    });
+                timerEmitter.emit('tick', timer.name, timer.payload);
+            }
+        }else{
+            const timer = await new Timer({name: `nu-${timerNu++}`, payload: TimerHandler.rangeToUnix(range)});
             timerEmitter.emit('tick', timer.name, timer.payload);
         }
-    }else{
-        const timer = await new Timer({name: `nu-${timerNu++}`, payload: TimerHandler.rangeToUnix(range)});
-        timerEmitter.emit('tick', timer.name, timer.payload);
     }
+
+    run();
+} else {
+    console.error('Неправильно заданы параметры (node ./hw2_2.js 1-0-0-0) || (node ./hw2_2.js "1-0-0-0 2-0-0-0")');
 }
 
-run();
+
